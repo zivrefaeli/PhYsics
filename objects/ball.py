008a7a2c66b339a3408399a73e2769c980731d41
refaeli <refaeliziv@gmail.com>
2022-12-18 15:44:09 +0200

    create ball class

---
+++ objects/ball.py
@@ -0,0 +1,79 @@
+from json import dumps
+from pygame import surface, draw
+from .dot import Dot
+from .vector import Vector
+from .constants import BLACK, GRAY, GREEN, BLUE, HEIGHT, WIDTH, GRAVITY
+from .luncher import Luncher
+from .equation import Equation
+
+
+class Ball:
+    RADIUS = 25
+    RANGE = 5 * RADIUS
+    WALLS_FRICTION = 0.5
+
+    def __init__(self) -> None:
+        self.position = Dot(self.RADIUS, self.RADIUS + HEIGHT / 2)
+        self.a = Vector(GRAVITY, 270)
+        self.v = Vector()
+
+        self.luncher = Luncher(self.position, self.RANGE)
+        self.lunched = False
+
+        self.equation = Equation(self.luncher.vector, self.position, BLUE)
+        self.path = Equation(self.v, self.position, GREEN)
+
+    def update(self) -> None:
+        if self.position.y <= self.RADIUS and not self.lunched:
+            self.position.y = self.RADIUS
+            self.v.size = 0
+            return
+
+        self.lunched = False
+        self.v += self.a
+        
+        vx, vy = self.v.delta()
+        self.position.x += vx
+        self.position.y += vy
+
+    def display(self, window: surface.Surface, mouse: tuple[float, float]) -> None:
+        self.update()
+
+        x, y = self.position.convert()
+        draw.circle(window, GRAY, (x, y), self.RANGE)
+        draw.circle(window, BLACK, (x, y), self.RADIUS)
+        
+        self.luncher.display(window, mouse)
+        self.equation.display(window)
+        
+        self.path.vector = self.v
+        self.path.display(window)
+
+    def lunch(self) -> None:
+        self.lunched = True
+        self.v += self.luncher.vector
+
+    def bounce(self, x1: float = 0, x2: float = WIDTH) -> None:
+        bounced = False
+        x1 += self.RADIUS
+        x2 -= self.RADIUS
+
+        if self.position.x < x1:
+            self.position.x = x1
+            bounced = True
+        elif self.position.x > x2:
+            self.position.x = x2
+            bounced = True
+
+        if bounced:
+            self.v.angle = 180 - self.v.angle
+            self.v.size *= self.WALLS_FRICTION
+
+    def __str__(self) -> str:
+        return dumps({
+            'position': str(self.position),
+            'a': str(self.a),
+            'v': str(self.v),
+            'luncher': str(self.luncher),
+            'equation': str(self.equation)
+        }, indent=2)
