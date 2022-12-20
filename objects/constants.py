85c2b954b16e806f32ca7b5b6de5625ec4f05219
refaeli <refaeliziv@gmail.com>
2022-12-21 01:11:58 +0200

    add electricity constants

--- objects/constants.py
+++ objects/constants.py
@@ -1,6 +1,10 @@
+import pygame
+pygame.init()
+
 # Measurements 
 WIDTH, HEIGHT = 800, 600
-DECIMAL_DIGITS = 5
+FLOAT_DIGITS = 5
+FPS = 60
 
 # Colors
 BLACK = (0, 0, 0)
@@ -9,6 +13,10 @@ RED = (255, 0, 0)
 GREEN = (74, 224, 83)
 BLUE = (0, 0, 255)
 GRAY = (240, 240, 240)
+GREY = (200, 200, 200)
 
 # Physics
 GRAVITY = 0.5
+k = 9 * 10 ** 9
+e = 1.6 * 10 ** -19
+Me = 9.11 * 10 ** -31
