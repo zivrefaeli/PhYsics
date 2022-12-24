2e91b00390159e7044300624e5ecdb32e9911395
refaeli <refaeliziv@gmail.com>
2022-12-24 23:55:34 +0200

    edit objects exports

--- objects/__init__.py
+++ objects/__init__.py
@@ -1,3 +1,5 @@
 from .constants import *
 from .ballistics.ball import Ball
 from .electricity.particle import Particle, Electron, Proton
+from .electricity.rainbow import Rainbow
+from .electricity.magnetic import MagneticField
