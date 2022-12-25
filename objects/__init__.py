f871f8f01df77818c28914b30b554d8919cd23f0
refaeli <refaeliziv@gmail.com>
2022-12-25 20:49:29 +0200

    export more objects

--- objects/__init__.py
+++ objects/__init__.py
@@ -1,4 +1,7 @@
 from .constants import *
+from .methods import Methods
+from .dot import BallisticsDot, ElectricityDot
+from .vector import Vector
 from .ballistics.ball import Ball
 from .electricity.particle import Particle, Electron, Proton
 from .electricity.rainbow import Rainbow
