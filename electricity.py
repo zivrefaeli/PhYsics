f3db2e22829be94647d37af35197f02818082845
refaeli <refaeliziv@gmail.com>
2022-12-31 23:38:57 +0200

    applying ElectricBoard

--- electricity.py
+++ electricity.py
@@ -1,6 +1,6 @@
 import pygame
-from pygame import display, time, event, draw, mouse, font
-from objects import BLACK, WIDTH, HEIGHT, WHITE, FPS, Methods, ElectricityDot as Dot, Vector, Particle, Electron, Proton, MagneticField, Rainbow
+from pygame import display, time, event, draw, mouse, font, key
+from objects import BLACK, WIDTH, HEIGHT, WHITE, FPS, Methods, ElectricityDot as Dot, Vector, Particle, Electron, Proton, MagneticField, ElectricBoard, Rainbow
 
 CHARGE_RANGE = (1, 5)
 AXES_VECTOR = Vector()
@@ -8,7 +8,7 @@ AXES_VALUES = [(Dot(WIDTH / 2, 0), 0), (Dot(0, HEIGHT / 2), 90)]
 INFO_NAMES = ['Position', 'a', 'v']
 INFO_FONT = font.SysFont('Consolas', 14)
 PADDING = 10
-INSIDE, OUTSIDE = 'inside', 'outside'
+INSIDE, OUTSIDE, POSITIVE, NEGATIVE, MAGNETIC, ELECTRIC = 'inside', 'outside', 'positive', 'negative', 'Magnetic', 'Electric'
 
 window = display.set_mode((WIDTH, HEIGHT))
 running = True
@@ -19,13 +19,14 @@ particles: list[Particle] = []
 charge = CHARGE_RANGE[0]
 
 fields: list[MagneticField] = []
-create_field, origin_selected, field_direction, shift_click = False, False, False, False
-radius_vector = Vector()
+boards: list[ElectricBoard] = []
+create_field, origin_selected, field_direction, is_magnetic, shift_clicked = False, False, False, False, False
+field_vector = Vector()
 field_origin = Dot()
 
 
 def handle_events() -> None:
-    global running, charge, create_field, origin_selected, field_direction, shift_click
+    global running, charge, create_field, origin_selected, field_direction, is_magnetic, shift_clicked
 
     for e in event.get():
         if e.type == pygame.QUIT:
@@ -34,25 +35,27 @@ def handle_events() -> None:
 
         elif e.type == pygame.KEYDOWN:
             if e.key == pygame.K_c:
-                if shift_click:
+                if shift_clicked:
                     fields.clear()
+                    boards.clear()
                 else:
                     particles.clear()
 
-            elif e.key == pygame.K_m:
+            elif e.key == pygame.K_m or e.key == pygame.K_e:
                 create_field = not create_field
                 origin_selected = False
+                is_magnetic = e.key == pygame.K_m
 
             elif e.key == pygame.K_s:
                 if create_field:
                     field_direction = not field_direction
 
             elif e.key == pygame.K_LSHIFT:
-                shift_click = True
+                shift_clicked = True
 
         elif e.type == pygame.KEYUP:
             if e.key == pygame.K_LSHIFT:
-                shift_click = False
+                shift_clicked = False
 
         elif e.type == pygame.MOUSEBUTTONDOWN:
             mx, my = e.pos
@@ -62,11 +65,16 @@ def handle_events() -> None:
                     continue
                 mx, my = Dot.convert_from(mx, my)
                 if origin_selected:
-                    radius = Dot(mx, my).distance(field_origin)
-                    if radius > particle.RADIUS:
+                    second_dot = Dot(mx, my)
+                    radius = second_dot.distance(field_origin)
+
+                    if is_magnetic and radius > particle.RADIUS:
                         fields.append(MagneticField(Dot(field_origin.x, field_origin.y), radius, field_direction))
-                        origin_selected = False
-                        create_field = False
+                    elif not is_magnetic and radius > particle.RADIUS * 4:
+                        boards.append(ElectricBoard(Dot(field_origin.x, field_origin.y), second_dot, field_direction))
+
+                    origin_selected = False
+                    create_field = False
                 else:
                     field_origin.x = mx
                     field_origin.y = my
@@ -106,8 +114,9 @@ def display_info() -> None:
     window.blit(charge_text, charge_text.get_rect(bottomright=(WIDTH - PADDING, HEIGHT - PADDING)))
 
     if create_field:
-        direction = INSIDE if field_direction else OUTSIDE
-        create_field_text = INFO_FONT.render(f'Creating Field: {direction}', True, BLACK)
+        direction = (INSIDE if field_direction else OUTSIDE) if is_magnetic else (POSITIVE if field_direction else NEGATIVE)
+        field_type = MAGNETIC if is_magnetic else ELECTRIC
+        create_field_text = INFO_FONT.render(f'Creating {field_type} Field: {direction}', True, BLACK)
         window.blit(create_field_text, create_field_text.get_rect(bottomleft=(PADDING, HEIGHT - PADDING)))
 
 
@@ -142,9 +151,11 @@ def electricity() -> None:
         if create_field and origin_selected:
             mx, my = mouse.get_pos()
             cx, cy = Dot.convert_from(mx, my)
-            radius_vector.angle = Methods.get_angle_by_delta(cx - field_origin.x, cy - field_origin.y)
-            radius_vector.display(window, BLACK, field_origin, (mx, my))
+            field_vector.angle = Methods.get_angle_by_delta(cx - field_origin.x, cy - field_origin.y)
+            field_vector.display(window, BLACK, field_origin, (mx, my))
 
+        for board in boards:
+            board.display(window)
         for field in fields:
             field.display(window)
         for _particle in particles:
@@ -153,7 +164,7 @@ def electricity() -> None:
         for x, y, color in path:
             draw.circle(window, color, (x, y), 1)
 
-        particle.apply_forces(particles, fields)
+        particle.apply_forces(particles + boards, fields)
         particle.display(window)
 
         if particle.v.size > 0:
--- objects/electricity/particle.py
+++ objects/electricity/particle.py
@@ -1,5 +1,6 @@
 from pygame import surface, draw, font
 from .magnetic import MagneticField
+from .board import ElectricBoard
 from ..constants import k, e, Me, BLUE, RED, GREY, WHITE, LIGHTBLUE, GREEN, WIDTH, HEIGHT
 from ..methods import Methods
 from ..dot import ElectricityDot as Dot
@@ -50,33 +51,48 @@ class Particle:
             self.a.display(window, LIGHTBLUE, self.position, self.RADIUS * 30)
             self.v.display(window, GREEN, self.position, self.RADIUS * 2)
 
-    def apply_forces(self, particles: list, fields: list) -> None:
-        self.apply_electric_force(particles)
+    def apply_forces(self, objects: list, fields: list) -> None:
+        self.apply_electric_force(objects)
         self.apply_magnetic_force(fields)
 
-    def apply_electric_force(self, particles: list) -> None:
+    def apply_electric_force(self, objects: list) -> None:
         self.Etotal.reset()
 
-        for particle in particles:
-            if not isinstance(particle, Particle):
+        for obj in objects:
+            if isinstance(obj, Particle):
+                particle = obj
+                r = particle.position.distance(self.position)
+                if r == 0:
+                    continue
+                Esize = k * abs(particle.q) / r ** 2 # E = kQ/r^2
+
+                Eangle = Methods.get_angle_by_delta(self.position.x - particle.position.x, self.position.y - particle.position.y)
+                if particle.q < 0:
+                    Eangle += 180
+            elif isinstance(obj, ElectricBoard):
+                board = obj
+                if not board.in_field(self.position):
+                    continue
+                Esize = board.E
+
+                Eangle = board.angle
+                if board.angle == 0:
+                    board_x = board.start.x
+                    if board.positive and self.position.x <= board_x or \
+                        not board.positive and self.position.x >= board_x:
+                        Eangle += 180
+                elif board.positive and board.l_board.below(self.position) or \
+                    not board.positive and board.l_board.above(self.position):
+                    Eangle += 180
+            else:
                 continue
-
-            r = particle.position.distance(self.position)
-            if r == 0:
-                self.reset()
-                return
-            Esize = abs(k * particle.q / r ** 2) # E = kQ/r^2
-
-            Eangle = Methods.get_angle_by_delta(self.position.x - particle.position.x, self.position.y - particle.position.y)
-            if particle.q < 0:
-                Eangle += 180
 
             self.vector.size = Esize
             self.vector.angle = Eangle
 
             self.Etotal += self.vector
 
-        angle = 180 if self.q < 0 and len(particles) > 0 else 0
+        angle = 180 if self.q < 0 and len(objects) > 0 else 0
 
         self.Fe.size = abs(self.q) * self.Etotal.size # F = qE
         self.Fe.angle = self.Etotal.angle + angle
@@ -110,6 +126,9 @@ class Particle:
     def in_bounds(self) -> bool:
         x, y = self.position.convert()
         return 0 <= x <= WIDTH and 0 <= y <= HEIGHT
+
+    def __str__(self) -> str:
+        return str(self.q)
 
 
 class Electron(Particle):
